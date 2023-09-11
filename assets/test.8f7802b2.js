009a6e574a4e8157eed1768ba01c1a44de7821de
catan271 <canh.tranduc2002@gmail.com>
2023-09-11 14:14:45 +0700

    Updates

--- assets/test.8f7802b2.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./nuxt-link.64c35062.js";import{_ as o,c as s,b as c,w as a,a as n,o as _,d as r}from"./entry.3e8f4391.js";const d={},i=n("div",{class:"p-50"},"test",-1);function m(f,l){const e=t;return _(),s("div",null,[i,c(e,{to:"/"},{default:a(()=>[r("To home")]),_:1})])}const x=o(d,[["render",m]]);export{x as default};
